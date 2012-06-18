8b257b3aaee79a0b49f1c19ee8187543de5f9502
Sam Frances <sam@samfrances.co.uk>
2012-06-18 21:34:29 +0100

    Added '__unicode__' methods to blog models classes

--- models.py
+++ models.py
@@ -3,9 +3,15 @@ from datetime import datetime
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    
+    def __unicode__(self):
+        return self.name
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     publication_date = models.DateField(default=datetime.now)
     body = models.TextField()
     tags = models.ManyToManyField(Category)
+    
+    def __unicode__(self):
+        return self.title
