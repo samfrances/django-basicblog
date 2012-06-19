ad26d24ac21d6066b5bd92237a2bb07a93b7e091
Sam Frances <sam@samfrances.co.uk>
2012-06-19 22:40:42 +0100

    Basic blog functionality established. Main blog view.

--- views.py
+++ views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.views.generic.dates import ArchiveIndexView
+from portfolio.blog.models import Post, Category
+
+class BlogHome(ArchiveIndexView):
+    queryset = Post.objects.all()
+    paginate_by = 10
+    date_field = 'publication_date'
+    template_name = 'blog.html'
+    context_object_name = 'posts'
+    pagename = 'blog'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogHome, self).get_context_data(**kwargs)
+        context['pagename'] = self.pagename
+        return context
