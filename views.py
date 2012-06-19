93df09aa897de30a28dadfffeea6ef36a0b33672
Sam Frances <sam@samfrances.co.uk>
2012-06-19 22:59:34 +0100

    Changed view architecture. Adding pagename to context is now acheived using a mixin: portfolio.views._BasicViewMixin

--- views.py
+++ views.py
@@ -1,15 +1,11 @@
 from django.views.generic.dates import ArchiveIndexView
 from portfolio.blog.models import Post, Category
+from portfolio.views import _BasicViewMixin
 
-class BlogHome(ArchiveIndexView):
+class BlogHome(_BasicViewMixin, ArchiveIndexView):
     queryset = Post.objects.all()
     paginate_by = 10
     date_field = 'publication_date'
     template_name = 'blog.html'
     context_object_name = 'posts'
     pagename = 'blog'
-
-    def get_context_data(self, **kwargs):
-        context = super(BlogHome, self).get_context_data(**kwargs)
-        context['pagename'] = self.pagename
-        return context
