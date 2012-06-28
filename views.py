ba8ec2cfa2f5de931598c15baa745f9caa6074db
Sam Frances <sam@samfrances.co.uk>
2012-06-28 13:23:44 +0100

    Removed redundant modifications in class-based generic date-based views

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ from django.http import Http404
 from django.template.defaultfilters import slugify
 
 class BlogHome(_BasicViewMixin, ArchiveIndexView):
-    queryset = Post.objects.all()
+    model = Post
     paginate_by = 10
     date_field = 'publication_date'
     template_name = 'blog/home.html'
@@ -15,13 +15,7 @@ class BlogHome(_BasicViewMixin, ArchiveIndexView):
     pagename = 'blog'
 
 class SinglePost(_BasicViewMixin, DateDetailView):
-    def get_object(self, queryset=None):
-        year, month, day = self.get_year(), self.get_month(), self.get_day() 
-        year, month, day = int(year), int(month), int(day)
-        slug = self.kwargs['slug']
-        pk = int(self.kwargs['pk'])
-        ob = get_object_or_404(Post, publication_date=datetime(year, month, day), slug__exact=slug, id=pk)
-        return ob
+    model = Post
     date_field = 'publication_date'
     month_format = '%m'
     pagename = 'blog'
