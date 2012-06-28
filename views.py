e9da19555be66265b29b912b2d2827061e1a3475
Sam Frances <sam@samfrances.co.uk>
2012-06-28 18:57:39 +0100

    Added sidebar month archive links. Acheived with _MonthListMixin that modifies get_context_data() to add a list of months to template context. Blog templates modified accordingly, adding an extra blog/base.html template between blog templates and the site-wide base.html template.

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.views.generic.dates import ArchiveIndexView, DateDetailView
+from django.views.generic.dates import ArchiveIndexView, DateDetailView, BaseDateListView
 from django.shortcuts import get_object_or_404
 from portfolio.blog.models import Post, Category
 from portfolio.views import _BasicViewMixin
@@ -6,7 +6,13 @@ from datetime import datetime
 from django.http import Http404
 from django.template.defaultfilters import slugify
 
-class BlogHome(_BasicViewMixin, ArchiveIndexView):
+class _MonthListMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(_MonthListMixin, self).get_context_data(**kwargs)
+        context['months'] = self.get_queryset().dates(self.get_date_field(), 'month', order="DESC")
+        return context
+
+class BlogHome(_MonthListMixin, _BasicViewMixin, ArchiveIndexView):
     model = Post
     paginate_by = 10
     date_field = 'publication_date'
@@ -14,7 +20,7 @@ class BlogHome(_BasicViewMixin, ArchiveIndexView):
     context_object_name = 'posts'
     pagename = 'blog'
 
-class SinglePost(_BasicViewMixin, DateDetailView):
+class SinglePost(_MonthListMixin, _BasicViewMixin, DateDetailView,):
     model = Post
     date_field = 'publication_date'
     month_format = '%m'
