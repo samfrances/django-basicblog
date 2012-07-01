4a3eb27fafbe142b65d9a1511ba6f8260f0b4e6a
Sam Frances <sam@samfrances.co.uk>
2012-07-01 22:00:08 +0100

    Debugged Category name validation

--- validators.py
+++ validators.py
@@ -4,7 +4,7 @@ from string import ascii_lowercase, digits
 def characterValidator(strng):
     def validator(value):
         for char in value:
-            if value not in strng:
+            if char not in strng:
                 raise ValidationError(u"%s contains characters not in '%s'" % (value, strng))
     return validator
 
