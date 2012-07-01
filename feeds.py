ee324a17f2a66e5a4ccaf66b240f2a97e000a297
Sam Frances <sam@samfrances.co.uk>
2012-07-01 21:18:42 +0100

    RSS Feeds

---
+++ feeds.py
@@ -0,0 +1,28 @@
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
+from portfolio.blog.models import Post
+from datetime import tzinfo, timedelta
+
+class RssFeed(Feed):
+    title = "Sam Frances's blog"
+    link = '/blog/'
+    description = 'Blog updates from samfrances.co.uk'
+   
+    def items(self):
+        return Post.objects.order_by('-publication_date')
+    
+    def item_title(self, item):
+        return item.title
+    
+    def item_description(self, item):
+        return item.body
+    
+    def item_link(self, item):
+        return item.url
+    
+    def item_pubdate(self, item):
+        return item.publication_date
+
+class AtomFeed(RssFeed):
+    feed_type = Atom1Feed
+    subtitle = RssFeed.description
