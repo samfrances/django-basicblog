2b1b23b68714b3ebea36839d2e8f8665d7d2fc80
Sam Frances <sam@samfrances.co.uk>
2012-07-08 14:42:32 +0100

    Changed get_blog_vars syntax to {% get_blog_vars as [var_name] %}

--- templatetags/blog_extras.py
+++ templatetags/blog_extras.py
@@ -1,18 +1,32 @@
 from django import template
 from portfolio.blog.models import Post, Category
+import re
 
 register = template.Library()
 
 @register.tag
 def get_blog_vars(parser, token):
-    args = token.contents.split()
-    if len(args) > 1:
-        raise template.TemplateSyntaxError("%r tag does not take any arguments" % args[0])
-    return BlogVarsNode()
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires arguments" % token.contents.split()[0])
+    m = re.search(r"as (\w+)", arg)
+    if not m:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+    var_name = m.groups()[0]
+    return BlogVarsNode(var_name)
 
 class BlogVarsNode(template.Node):
+    
+    def __init__(self, var_name):
+        self.var_name = var_name
+
     def render(self, context):
-        context['blog'] = dict()
-        context['blog']['months'] = Post.objects.all().dates('publication_date', 'month', order="DESC")
-        context['blog']['categories'] = Category.objects.all()
+        blog_vars = dict()
+        blog_vars['months'] = Post.objects.all().dates('publication_date', 'month', order="DESC")
+        blog_vars['categories'] = Category.objects.all()
+        context[self.var_name] = blog_vars
+#        context['blog'] = dict()
+#        context['blog']['months'] = Post.objects.all().dates('publication_date', 'month', order="DESC")
+#        context['blog']['categories'] = Category.objects.all()
         return ''
