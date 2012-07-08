4109e5f23c8e0707f436c02e409e3af69d462822
Sam Frances <sam@samfrances.co.uk>
2012-07-08 13:27:17 +0100

    Removed inheritance from ExtraContextMixin from blog views. No need for this mixin, as roles have been taken over by new template tags for settings context variables and importing 'global' blog variables

--- views.py
+++ views.py
@@ -6,43 +6,23 @@ from datetime import datetime
 from django.http import Http404
 from django.template.defaultfilters import slugify
 
-class BlogBase(ExtraContextMixin):
-    """Base class for blog posts. Provides the functionality of ExtraContextMixin and augments the context with a list
-    of all blog post categories, all months for which there are blog articles, and other items"""
- 
-
-    paginate_by = 5
-
-    def get_extra_context(self):
-        categories = Category.objects.all()
-        months = Post.objects.all().dates(self.get_date_field(), 'month', order="DESC")
-        try:
-            extra_context = self.extra_context
-        except AttributeError:
-            extra_context = dict()
-        extra_context['categories'] = categories
-        extra_context['months'] = months
-#        extra_context['current_tab'] = 'blog'
-        return extra_context
-
-class BlogHome(BlogBase, ArchiveIndexView):
+class BlogBase(object):
+    """Base class for blog posts."""
     model = Post
+    paginate_by = 5
     date_field = 'publication_date'
-    template_name = 'blog/home.html'
     context_object_name = 'posts'
 
+class BlogHome(BlogBase, ArchiveIndexView):
+    template_name = 'blog/home.html'
+
 class SinglePost(BlogBase, DateDetailView,):
-    model = Post
-    date_field = 'publication_date'
     month_format = '%m'
     context_object_name = 'post'
     template_name = 'blog/singlepost.html'
 
 class MonthBlogView(BlogBase, MonthArchiveView):
-    model = Post
-    date_field = 'publication_date'
     month_format = '%m'
-    context_object_name = 'posts'
     template_name = 'blog/month_archive.html'
     
     def get_queryset(self):
