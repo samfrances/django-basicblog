459a740496cfccd15367e21584852aca56c0f9f9
Sam Frances <sam@samfrances.co.uk>
2012-07-08 12:25:21 +0100

    custom template tag 'get_blog_vars' working

--- templatetags/blog_extras.py
+++ templatetags/blog_extras.py
@@ -1,19 +1,18 @@
 from django import template
-from blog.models import Post, Category
+from portfolio.blog.models import Post, Category
 
 register = template.Library()
 
+@register.tag
 def get_blog_vars(parser, token):
     args = token.contents.split()
-    if len(args) > 1: except ValueError:
+    if len(args) > 1:
         raise template.TemplateSyntaxError("%r tag does not take any arguments" % args[0])
     return BlogVarsNode()
 
 class BlogVarsNode(template.Node):
-    def __init__(self):
-        self.vars = dict()
-        self.vars["categories"] = Category.objects.all()
-        self.vars["months"] = Post.objects.all().dates(self.get_date_field(), 'month', order="DESC")
     def render(self, context):
-        context['blog'] = self.vars
+        context['blog'] = dict()
+        context['blog']['months'] = Post.objects.all().dates('publication_date', 'month', order="DESC")
+        context['blog']['categories'] = Category.objects.all()
         return ''
