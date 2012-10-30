865f4aecac0c87413fa9318609c23c5db55abd34
Sam Frances <sam@samfrances.co.uk>
2012-10-30 22:44:38 +0000

    test changes

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # name urlpattern used because otherwise the generic view confuses the permalink decorator in the Post model
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>.+)/$', SinglePost.as_view(), name='single_post_url'),
     (r'^$', BlogHome.as_view()),
-    (r'^tag/(?P<tag>[A-Za-z0-9]+)/$', CategoryBlogView.as_view()),
+    (r'^tag/(?P<tag>[A-Za-z]+)/$', CategoryBlogView.as_view()),
     (r'^feeds/rss/$', RssFeed()),
     (r'^feeds/atom/$', AtomFeed()), 
     (r'^comments/', include('django.contrib.comments.urls')),
