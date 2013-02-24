345eb932a0f353b605e4e70270354f46a892a8e7
Sam Frances <sam@samfrances.co.uk>
2013-02-24 18:58:31 +0000

    added basic setup setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
 from distribute_setup import use_setuptools
 use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django Basic Blog", # Check other projects to see if name is same as package
+    version = "0.1",
+    packages = find_packages(),
+)
